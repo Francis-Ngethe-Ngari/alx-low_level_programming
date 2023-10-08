6a9c7f3b63bb102d01762e43a490cedc1e248a71
Francis_Vagrant <francisngari@gmail.com>
2023-10-08 11:30:27 +0000

    Project tasks in c which show compilation steps

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*Using puts method to ouput a string*/
+	puts("\"Programming is like building a multilingual puzzle");
+
+	getchar();
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,5 @@
+### **C Scripts Samples**
+
+0-preprocessor script executes the preprocessor step of making a file executable and stops after the process is done due to -E arguement 
+
+added,the output of preprocessor step is saved to an ouput file.
