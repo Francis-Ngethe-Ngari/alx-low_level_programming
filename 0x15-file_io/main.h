55cbd48addb694bff29b2902b0495cd4e60622da
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2023-11-01 15:18:57 +0300

    Addded prototype to handle print error message in 100-elf_header.c script

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,8 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void exit_error(int exit_code, const char *f_str, const char *Arg);
-void exit_with_err(const char *msg);
-void output_elf_hedr(Elf32_Ehdr *hedr);
+void ext_err(const char *msg);
+int main(int argc, char *argv[]);
 int _putchar(int c);
 
 #endif
